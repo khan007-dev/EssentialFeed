b2a95b6af1f5e286c9113e27de98169a63bcc3e8
abdulwariskhan <hellokhan.ch@gmail.com>
2024-08-01 23:34:23 +0200

    Create FeedLoader Boundery

--- EssentialFeed/FeedLoader.swift
+++ EssentialFeed/FeedLoader.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error (Error)
+}
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
